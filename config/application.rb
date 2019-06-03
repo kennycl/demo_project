7ec003a30db643584d47b87251702cf8915ca903
Kenny Claustro <kennyclaustro@gmail.com>
2019-06-03 09:05:02 -0700

    bug fixxes

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,3 @@ module DemoProject
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-end
