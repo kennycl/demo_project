06cd35c4d88263077572f682101d8ee78c92f868
Kenny Claustro <kennyclaustro@gmail.com>
2019-06-03 08:20:17 -0700

    bug fixxes

--- config/application.rb
+++ config/application.rb
@@ -13,13 +13,13 @@ module DemoProject
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
-      end
+        
 
       # Load application's view overrides
       Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
+        
+      
 
  config.assets.initialize_on_precompile = false
  
