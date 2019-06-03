8c347690cb9f721991da1c2ce5372e1e53660411
Kenny Claustro <kennyclaustro@gmail.com>
2019-06-03 08:17:41 -0700

    bug fixxes

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -14,15 +14,15 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
-end
+
 
 Paperclip.interpolates(:s3_eu_url) do |attachment, style|
 "#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
-end
+
 
 Spree.user_class = "Spree::User"
 
-Spree.config do |config|
+
   config.use_s3 = true
   config.s3_bucket = '<hikennethy>'
   config.s3_access_key = "<AKIAI47HMXASRYQXVLRA>"
